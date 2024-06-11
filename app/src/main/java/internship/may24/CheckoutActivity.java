52996088a59776a2bea4698f940dc232c6b68b90
Sagar Khatri <khatrisagar2@gmail.com>
2024-06-11 17:39:50 +0530

    Razorpay Payment Gateway

--- app/src/main/java/internship/may24/CheckoutActivity.java
+++ app/src/main/java/internship/may24/CheckoutActivity.java
@@ -1,10 +1,12 @@
 package internship.may24;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,7 +24,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CheckoutActivity extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultListener;
+import com.razorpay.PaymentResultWithDataListener;
+
+import org.json.JSONObject;
+
+public class CheckoutActivity extends AppCompatActivity implements PaymentResultWithDataListener {
 
     EditText name, email, contact, address, pincode;
     Button checkout;
@@ -163,9 +172,78 @@ public class CheckoutActivity extends AppCompatActivity {
                         new CommonMethod(CheckoutActivity.this, DashboardActivity.class);
                         finish();
                     }
+                    else{
+                        startPayment();
+                    }
                 }
             }
         });
 
+        //startPayment();
+
+    }
+
+    private void startPayment() {
+        /**
+         * Instantiate Checkout
+         */
+        Checkout checkout = new Checkout();
+
+        checkout.setKeyID("rzp_test_xsiOz9lYtWKHgF");
+
+        /**
+         * Reference to current activity
+         */
+        final Activity activity = this;
+
+        /**
+         * Pass your payment options to the Razorpay Checkout as a JSONObject
+         */
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", getResources().getString(R.string.app_name));
+            options.put("description", "Reference No. #123456");
+            options.put("send_sms_hash", true);
+            options.put("allow_rotation", true);
+            options.put("image", R.drawable.icon);
+            //options.put("order_id", "order_DBJOWzybf0sJbb");//from response of step 3.
+            //options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+            options.put("amount", String.valueOf(Integer.parseInt(sp.getString(ConstantSp.TOTAL_AMOUNT,""))*100));//pass amount in currency subunits
+            options.put("prefill.email", sp.getString(ConstantSp.EMAIL,""));
+            options.put("prefill.contact",sp.getString(ConstantSp.CONTACT,""));
+            JSONObject retryObj = new JSONObject();
+            //retryObj.put("enabled", true);
+            //retryObj.put("max_count", 4);
+            //options.put("retry", retryObj);
+            checkout.open(activity, options);
+
+        } catch(Exception e) {
+            Log.e("RESPONSE_PAYMENT_CATCH", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s, PaymentData paymentData) {
+        Log.d("RESPONSE_PAYMENT_SUCCESS",s);
+        String insertQuery = "INSERT INTO TBL_ORDER VALUES(NULL,'" + sp.getString(ConstantSp.USERID, "") + "','" + name.getText().toString() + "','" + email.getText().toString() + "','" + contact.getText().toString() + "','" + address.getText().toString() + "','" + sCity + "','" + pincode.getText().toString() + "','" + sPayVia + "','"+s+"','" + sp.getString(ConstantSp.TOTAL_AMOUNT, "") + "')";
+        db.execSQL(insertQuery);
+
+        String selectQuery = "SELECT MAX(ORDERID) FROM TBL_ORDER LIMIT 1";
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if(cursor.getCount()>0){
+            while (cursor.moveToNext()){
+                String updateCartQuery = "UPDATE CART SET ORDERID='"+cursor.getString(0)+"' WHERE USERID='"+sp.getString(ConstantSp.USERID,"")+"' AND ORDERID='0'";
+                db.execSQL(updateCartQuery);
+            }
+        }
+        new CommonMethod(CheckoutActivity.this, "Order Placed Successfully");
+        new CommonMethod(CheckoutActivity.this, DashboardActivity.class);
+        finish();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s, PaymentData paymentData) {
+        Log.d("RESPONSE_PAYMENT_ERROR",s);
     }
 }
